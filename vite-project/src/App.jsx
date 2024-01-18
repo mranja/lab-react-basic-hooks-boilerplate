f53ac8c5e5e89cd515e26f564acee4f5706f96c7
ranjan <ranjan.m@kalvium.community>
2024-01-18 10:19:42 +0530

    Done

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css"
+import React, { useState} from "react";
+import Context from "./Component/Context";
+import Content from "./Component/Content";
+
+function App(){
+  return (
+    <>
+    <Context>
+      <Content />
+    </Context>
+    </>
+  )
+}
+
+export default App;
---
+++ vite-project/src/Component/Context.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { createContext, useState} from "react";
+
+export const MyContext = createContext()
+
+function Context({children}){
+    
+
+    let [darkTheme, setDarkTheme] = useState(false)
+    return (
+        <>
+        <MyContext.Provider value={{darkTheme, setDarkTheme}}>
+            {children}
+        </MyContext.Provider>
+        
+        </>
+    )
+}
+
+export default Context;
